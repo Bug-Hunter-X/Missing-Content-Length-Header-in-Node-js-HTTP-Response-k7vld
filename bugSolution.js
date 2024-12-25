b4de4c9147c0f4e10e13ff69eddeea48df03a0df
BugHunter-X <askzenai@gmail.com>
2024-12-25 18:34:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  const message = 'Hello, world!';
+  res.writeHead(200, {
+    'Content-Type': 'text/plain',
+    'Content-Length': Buffer.byteLength(message)
+  });
+  res.end(message);
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
